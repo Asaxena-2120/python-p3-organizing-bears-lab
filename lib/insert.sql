8f06d0b47442b5d3b20c9f75cd3eb47a98c91619
Aastha <saxena.aastha91@gmail.com>
2023-03-20 17:36:54 +0530

    passed all tests

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES (
+"Mr. Chocolate",3,'M',NULL,NULL,NULL);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES (
+"Rowdy",4,'F',NULL,NULL,NULL);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES (
+"Tabitha",5,'M',NULL,NULL,NULL);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES (
+"Sergeant Brown",6,'F',NULL,NULL,NULL);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES (
+"Melissa",7,'M',NULL,NULL,NULL);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES (
+"Grinch",8,'F',NULL,NULL,NULL);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES (
+"Wendy",9,'M',NULL,NULL,NULL);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES (
+NULL,10,'F',NULL,NULL,NULL);
